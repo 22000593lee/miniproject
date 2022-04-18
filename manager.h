d9d53c739fc10aaf6b94ef310d7927e2826be344
이하경 <22000593@handong.ac.kr>
2022-04-18 18:02:26 +0900

    검색함수들 추가 선언

---
+++ manager.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "product.h"
+
+void listProduct(Product *p, int count); // 전체 등록된 제품 리스트 출력
+int selectDataNo(Product *p, int count); //선택된 제품의 번호 출력
+int loadData(Product *p); // 저장된 제품 리스트 불러오기
+void saveData(Product *p, int count); // 제품 리스트 저장
+void searchProduct(Product *p, int count); // 제품이름 검색
