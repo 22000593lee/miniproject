a7afdfaa4e4e25674fc39dbfc12cc1799edbc211
이하경 <22000593@handong.ac.kr>
2022-04-18 18:00:43 +0900

    헤더에 검색기능3 함수 추가

--- product.h
+++ product.h
@@ -21,5 +21,7 @@ int selectDataNo(Product *p, int count); //선택된 제품의 번호 출력
 int loadData(Product *p); // 저장된 제품 리스트 불러오기
 void saveData(Product *p, int count); // 제품 리스트 저장
 void searchProduct(Product *p, int count); // 제품이름 검색
+void searchDelivery(Product *p, int count); //배송유형별 검색- 검색된 배송유형의 제품들 출력
+void searchPrice(Product *p, int count); //배송가격별 검색 - 검색된 가격 이하의 제품들  출력
 
 #endif
