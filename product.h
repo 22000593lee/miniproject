40000725265daef3242524d2e1f9fa6d59192e3b
이하경 <22000593@handong.ac.kr>
2022-04-18 18:21:17 +0900

    헤더 중복내용 삭제

--- product.h
+++ product.h
@@ -16,12 +16,5 @@ int createProduct(Product *p); // 제품 추가
 void readProduct(Product p); // 하나의 제품 출력
 int updateProduct(Product *p); // 제품 내용 수정
 int deleteProduct(Product *p); // 제품 삭제
-void listProduct(Product *p, int count); // 전체 등록된 제품 리스트 출력
-int selectDataNo(Product *p, int count); //선택된 제품의 번호 출력
-int loadData(Product *p); // 저장된 제품 리스트 불러오기
-void saveData(Product *p, int count); // 제품 리스트 저장
-void searchProduct(Product *p, int count); // 제품이름 검색
-void searchDelivery(Product *p, int count); //배송유형별 검색- 검색된 배송유형의 제품들 출력
-void searchPrice(Product *p, int count); //배송가격별 검색 - 검색된 가격 이하의 제품들  출력
 
 #endif
